a1e7e01176f6fea26b78926b5cdc66631cd1c500
task-masterr01 <hemant.here96@gmail.com>
2026-07-03 03:23:58 +0530

    add new data to p7

---
+++ P7/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import './App.css'
+
+
+const App = () => {
+  const [count, setCount] = useState(0) ;
+  const increaseCount = ()=>{
+    setCount ( count + 1) ;
+  }
+  const decreaseCount = ()=>{
+    setCount(count - 1) ;
+  }
+  
+  const resetCount = ()=>{
+    setCount(0) ;
+  }
+
+  return (
+    <main>
+        <h1> hello hemant welcome back</h1>
+         <h3> Total Count : {count}</h3>
+
+         <button onClick={increaseCount}>increse count</button>
+         <br />
+         <button onClick={decreaseCount}>decrese count</button>
+         <br />
+         <button onClick={resetCount}> Reset count</button>
+    </main>
+  )
+}
+
+export default App
